d0f3996c7cede20589e14af5bd73f1ab22b5e6c5
Andrew Rovenko <andrew.rovenko@gmail.com>
2017-01-25 21:25:41 +0200

    Markdown with checkboxes<

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Scratch project
 
-1. Create grabber
+- [x] Create grabber
     * Run [cheerio.js](https://www.npmjs.com/package/cheerio)
     * Copy simple picture from [dominos.ua](http://dominos.ua/)
     * Copy entire pizza element
-2. Push it into Mongo
-3. Output to page
+- [ ] Push it into Mongo
+- [ ] Output to page
--- index.js
+++ index.js
@@ -3,31 +3,20 @@ const rp = require ('request-promise');
 
 rp('http://dominos.ua/ru/Pizza/')
     .then(function (htmlString) {
-		var $ = cheerio.load(htmlString);
-    /*
-    var names = $('body > div.main > div.products_grid > div.product_item > p')
-      .text()
-      .split('\n')
-      .filter(el => el!=='');
-		console.log(names);
-    */
-    /*
-    var photos = $('body > div.main > div.products_grid > div.product_item > div.product_img_holder > a > img')
-      .attr('src');
-    console.log(photos);
-    */
-    var elements = [];
-    var ids = [];
-    var names = [];
-    var photos = [];
-    var ingredients = [];
+		let $ = cheerio.load(htmlString);
+
+    let elements = [];
     $('body > div.main > div.products_grid > div.product_item').each(function(i, elem){
       let link = this; //is it really faster ??
       let tmp = {};
-      tmp.id = $(link).attr('data-osg-pk');
-      tmp.name = cheerio.load(link)('p > a').text().replace(/\n/g,'');
-      tmp.photo = cheerio.load(link)('div.product_img_holder > a > img').attr('src');
-      tmp.ingredient = cheerio.load(link)('div.product_img_holder > div.product_mix > p')
+      tmp.id = $(link)
+        .attr('data-osg-pk');
+      tmp.name = cheerio.load(link)('p > a')
+        .text()
+        .replace(/\n/g,'');
+      tmp.photo = cheerio.load(link)('div.product_img_holder > a > img')
+        .attr('src');
+      tmp.ingredients = cheerio.load(link)('div.product_img_holder > div.product_mix > p')
         .text()
         .replace(/\n/g,'')
         .split(',');
